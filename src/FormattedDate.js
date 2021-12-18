a15c50658d0a37bf2747a9ad3ab9d651208667f8
alinaven <95190009+alinaven@users.noreply.github.com>
2021-12-18 20:20:25 +0100

    Format date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -13,17 +13,17 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.data.getDay()];
   let hours = props.date.getHours();
-  if (hours<10) {
-      hours=`0${hours}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
   let minutes = props.date.getMinutes();
-  if (minutes<10) {
-      minutes=`0${minutes};
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
   return (
     <div>
       {day} {hours}:{minutes}
     </div>
-  )
+  );
 }
